6a140be439c77702869ef76b5ecb73e76ff5f851
George Pasley <gpasley@imazuma.com>
2018-10-23 00:41:00 -0400

    Added user and article association and debugger view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+
+  has_many :articles
+
+  before_save { self.email = email.downcase }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 3..25}
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
