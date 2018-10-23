39f894578e24e3d5c4492d99842dd85f34735311
George Pasley <gpasley@imazuma.com>
2018-10-23 00:17:41 -0400

    user validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 3..25}
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
 
 end
