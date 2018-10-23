1c05d5fb54b530c8060a697e717945e1c44db88a
George Pasley <gpasley@imazuma.com>
2018-10-23 08:56:33 -0400

    add secure password and attribute to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 3..25}
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
 
+  has_secure_password
 end
